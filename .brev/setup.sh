c2d94c4110a111c20b57fea48bb7e678a069297b
Ali <ali.aka.ahmed@gmail.com>
2022-05-18 15:47:23 -0700

    Update setup.sh

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
+set -eo pipefail
 
 ####################################################################################
 ##### Specify software and dependencies that are required for this project     #####
