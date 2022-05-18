8d52e0e3f169b6b01a20f2443b2baaebfe8559a8
aedward8 <abigail.edwards317@gmail.com>
2022-05-18 22:08:46 +0000

    modified pkg-lock to rid fs-extra, added scripts

---
+++ .brev/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+####################################################################################
+##### Specify software and dependencies that are required for this project     #####
+#####    Brev.dev documentation: https://docs.brev.dev                         #####
+####################################################################################
+
+
+#### Node v14.x + npm #####
+(echo ""; echo "##### Node v14.x + npm #####"; echo "";)
+sudo apt install ca-certificates
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+####  Solidity  #####
+(echo ""; echo "##### Solidity #####"; echo "";)
+sudo apt-get install solc
+
+#### Next.js + (React) +react-dom #####
+(echo ""; echo "####Next.js + (React) +react-dom ####"; echo "";)
+npm install next react react-dom
+
+#### Project Setup #####
+(echo ""; echo "##### Project Setup #####"; echo "";)
+npm install 
